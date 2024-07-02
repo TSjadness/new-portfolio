ec58bf1cd89c28184efe44ad3837cdc6c8e4d865
Jadson Tavares <95153192+TSjadness@users.noreply.github.com>
2024-07-01 22:38:21 -0300

    update  home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    // colors: {
+    //   text: {
+    //     DEFAULT: "#fca61e",
+    //   },
+    // },
   },
   plugins: [],
 };
