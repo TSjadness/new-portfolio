b015475917367cbe4ce2c5429ef7206503692829
Jadson Tavares <95153192+TSjadness@users.noreply.github.com>
2024-08-28 01:29:20 -0300

    update

--- src/components/home/page.tsx
+++ src/components/home/page.tsx
@@ -4,22 +4,18 @@ import {
   DisclosureButton,
   DisclosurePanel,
   Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
   Transition,
 } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import ProjectList from '../projects/page';
+import { Bars3Icon,  XMarkIcon } from "@heroicons/react/24/outline";
 import aboutfoto from "@/images/logo-about.png";
 import Image from 'next/image';
+import photo from "@/images/foto-person.png"
 
 const user = {
   name: "Jadson Tavares Santos",
   email: "jadsonts2012@gmail.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
 };
+
 const navigation = [
   { name: "Home", href: "#home", current: false },
   { name: "Projetos", href: "#projetos", current: false },
@@ -27,10 +23,7 @@ const navigation = [
   { name: "Sobre mim", href: "#sobre-mim", current: false },
   { name: "Contato", href: "#contato", current: false },
 ];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
 
-];
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -78,27 +71,7 @@ export default function HomePage() {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
-                      {/* <button
-                        type="button"
-                        className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-[#fca61e] focus:ring-offset-2 focus:ring-offset-[#fed72f]"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
-                        <BellIcon className="h-6 w-6" aria-hidden="true" />
-                      </button> */}
-
                       <Menu as="div" className="relative ml-3">
-                        <div>
-                          {/* <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-[#fca61e] focus:ring-offset-2 focus:ring-offset-[#fed72f]">
-                            <span className="absolute -inset-1.5" />
-                            <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src={user.imageUrl}
-                              alt=""
-                            />
-                          </MenuButton> */}
-                        </div>
                         <Transition
                           enter="transition ease-out duration-100"
                           enterFrom="transform opacity-0 scale-95"
@@ -106,25 +79,7 @@ export default function HomePage() {
                           leave="transition ease-in duration-75"
                           leaveFrom="transform opacity-100 scale-100"
                           leaveTo="transform opacity-0 scale-95"
-                        >
-                          <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md py-1 shadow-lg ring-1 ring-[#fca61e] ring-opacity-5 focus:outline-none">
-                            {userNavigation.map((item) => (
-                              <MenuItem key={item.name}>
-                                {({ focus }) => (
-                                  <a
-                                    href={item.href}
-                                    className={classNames(
-                                      focus ? "hover:bg-gray-700" : "",
-                                      "block px-4 py-2 text-sm "
-                                    )}
-                                  >
-                                    {item.name}
-                                  </a>
-                                )}
-                              </MenuItem>
-                            ))}
-                          </MenuItems>
-                        </Transition>
+                        ></Transition>
                       </Menu>
                     </div>
                   </div>
@@ -170,10 +125,11 @@ export default function HomePage() {
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img
-                        className="h-10 w-10 rounded-full"
-                        src={user.imageUrl}
-                        alt=""
+                      <Image
+                        src={photo}
+                        width={50}
+                        height={50}
+                        alt="Picture of the author"
                       />
                     </div>
                     <div className="ml-3">
@@ -184,38 +140,12 @@ export default function HomePage() {
                         {user.email}
                       </div>
                     </div>
-                    <button
-                      type="button"
-                      className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                    >
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">View notifications</span>
-                      <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-                  <div className="mt-3 space-y-1 px-2">
-                    {userNavigation.map((item) => (
-                      <DisclosureButton
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                      >
-                        {item.name}
-                      </DisclosureButton>
-                    ))}
                   </div>
                 </div>
               </DisclosurePanel>
             </>
           )}
         </Disclosure>
-
-        {/* <header className="shadow text-[#fca61e]">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
-          </div>
-        </header> */}
       </div>
     </>
   );
