6f2442814504506f6324dfb13ded3bdfcdf7703c
jadson tavares santos <jadsontavarests@hotmail.com>
2024-09-11 02:13:48 -0300

    update

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,12 +2,22 @@
 
 import HomePage from "@/components/home/page";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export function AppRoutes() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<DashboardPage />} /> */}
         <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
