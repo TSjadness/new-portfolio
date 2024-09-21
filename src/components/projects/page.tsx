1c63ad08f706a66cb1f620179cd622fc5e84ae0b
jadson tavares santos <jadsontavarests@hotmail.com>
2024-09-21 13:34:30 -0300

    update new project

--- src/components/projects/page.tsx
+++ src/components/projects/page.tsx
@@ -4,6 +4,7 @@ import MasterixWeb from "@/image-projects/foto-Masterix.png";
 import AbrirMei from "@/image-projects/foto-AbriMei.png";
 import EmBreve from "@/image-projects/EmBreve.png";
 import ControleFinanca from "@/image-projects/controle-finanças.png";
+import GerenciadorTarefas from "@/image-projects/gerenciador-tarefas.png";
 
 
 
@@ -21,9 +22,9 @@ const Card = ({
   description: string;
 }) => (
   <div className="mx-3 mt-6 mb-6 flex flex-col self-start rounded-lg bg-[#2d2d2d] text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white sm:shrink-0 sm:grow sm:basis-0 max-w-md transform transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-10 ">
-    <a href={href}>
+    <a href={href} target='_blank'>
       <Image
-        className="rounded-t-lg max-h-80"
+        className="rounded-t-lg max-h-80 min-h-80"
         src={imgSrc}
         alt={imgAlt}
         width={600} 
@@ -69,6 +70,13 @@ const ProjectList = () => (
         imgAlt="Controle de Finanças"
         title="Controle de Finanças"
         description="Aplicação web que permite o controle de finanças pessoais de forma simples e intuitiva."
+      />
+      <Card
+        href="https://gerenciador-de-tarefas-olive.vercel.app/"
+        imgSrc={GerenciadorTarefas}
+        imgAlt="Gerenciador de Tarefas"
+        title="Controle de Finança"
+        description="Aplicação web que permite o gerenciamento de tarefas diárias de forma eficiente."
       />
       <Card
         href="#!"
